5e8437ff5c00b3055ae7d4c1438e8dfaf8498c25
TaxLawAdvisers <100040649+TaxLawAdvisers@users.noreply.github.com>
2022-02-20 05:35:52 +0530

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import * as React from "react";
+import * as ga from '../lib/ga'
+import { useRouter } from "next/router";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <Layout>
       <Component {...pageProps} />
